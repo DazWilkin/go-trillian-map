8196a9faf051f0c81249c47f163e788780edd3de
DazWilkin <daz.wilkin@gmail.com>
2020-09-07 09:42:52 -0700

    refactor

---
+++ cmd/server/examples.go
@@ -0,0 +1,12 @@
+package main
+
+// Examples is a set of key-value pairs for the Map
+var Examples = map[string]string{
+	"Freddie":  "Border Collie",
+	"Noisette": "Australian Shepherd",
+	"Artie":    "Weimeraner",
+	"Louie":    "Formosan Mountain Dog",
+	"Luna":     "Yorkiepoo",
+	"Bertie":   "Cockapoo",
+	"Mr Magoo": "Scottish Terrier",
+}
