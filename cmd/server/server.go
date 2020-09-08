7aa21bd078ca0f796752579e0ac1b025d7fcd4ae
DazWilkin <daz.wilkin@gmail.com>
2020-09-08 11:22:53 -0700

    More consistent Getter|Setter

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -23,6 +23,7 @@ func NewClient(client trillian.TrillianMapWriteClient, mapID int64) *Client {
 
 // Add is a function that adds leaves to a Map
 func (c *Client) Add(ctx context.Context, leaves []*trillian.MapLeaf, revision int64) error {
+	log.Print("[Client:Add] Entered")
 	rqst := &trillian.WriteMapLeavesRequest{
 		MapId:          c.mapID,
 		Leaves:         leaves,
@@ -37,13 +38,12 @@ func (c *Client) Add(ctx context.Context, leaves []*trillian.MapLeaf, revision i
 	return nil
 }
 
-// Get is a function that gets specific leaf-revisions from a Map
-func (c *Client) Get(ctx context.Context, leaf *trillian.MapLeaf, revision int64) ([]*trillian.MapLeaf, error) {
+// Get is a function that gets leaves for a specific revisions from a Map
+func (c *Client) Get(ctx context.Context, indexes [][]byte, revision int64) ([]*trillian.MapLeaf, error) {
+	log.Print("[Client:Get] Entered")
 	rqst := &trillian.GetMapLeavesByRevisionRequest{
-		MapId: c.mapID,
-		Index: [][]byte{
-			leaf.Index,
-		},
+		MapId:    c.mapID,
+		Index:    indexes,
 		Revision: revision,
 	}
 	resp, err := c.client.GetLeavesByRevision(ctx, rqst)
